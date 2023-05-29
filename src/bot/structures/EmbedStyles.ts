e8b6b6ceb809d1a3a9d6652b5dce25777b8d0806
Conqu3red <46159994+Conqu3red@users.noreply.github.com>
2023-05-29 13:04:52 +0100

    update icon url

--- src/bot/structures/EmbedStyles.ts
+++ src/bot/structures/EmbedStyles.ts
@@ -5,7 +5,7 @@ export const EMBED_COLOR = 0xfcab0d; /* 0x3586ff */
 export const BOT_NAME = "PB3 Leaderboards Bot";
 
 export const EMBED_IMAGE =
-    "https://cdn.discordapp.com/app-assets/720364938908008568/758752385244987423.png";
+    "https://cdn.discordapp.com/app-assets/1109186160468697181/1112704162438848592.png";
 
 export const EMBED_AUTHOR: APIEmbedAuthor = {
     name: BOT_NAME,
