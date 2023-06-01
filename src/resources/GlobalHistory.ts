e255735cbaaeba99f44b33762fb90d09af0f0aec
Conqu3red <46159994+Conqu3red@users.noreply.github.com>
2023-06-01 08:30:35 +0100

    june 1st global history data fix

--- src/resources/GlobalHistory.ts
+++ src/resources/GlobalHistory.ts
@@ -3,6 +3,7 @@ import { GlobalEntry, ScoringMode, globalLeaderboard } from "../GlobalLeaderboar
 import { LeaderboardType, OldestEntry } from "../LeaderboardInterface";
 import { database } from "./Lmdb";
 import { OLDEST_RANK_LIMIT } from "../Consts";
+import { cacheManager } from "./CacheManager";
 
 export interface GlobalHistoryEntry extends GlobalEntry {
     time: number; // epoch seconds
@@ -40,7 +41,9 @@ export class GlobalHistory {
                 if (top) {
                     const history = this.updateHistory(
                         this.get(type as LeaderboardType, mode as ScoringMode),
-                        top
+                        top,
+                        type as LeaderboardType,
+                        mode as ScoringMode
                     );
                     await this.set(type as LeaderboardType, mode as ScoringMode, history);
                     console.log(`[CacheManager] Global History History ${type} / ${mode} updated.`);
@@ -52,8 +55,28 @@ export class GlobalHistory {
         await database.put("globalt", this.lastReloadTimeMs);
     }
 
-    static updateHistory(history: GlobalHistoryEntry[], newTop: GlobalEntry[]) {
+    static JUNE_1ST_FILTER = 1400;
+    static JUNE_1ST_CUTOFF = DateTime.fromISO("2023-06-05").toSeconds();
+
+    static updateHistory(
+        history: GlobalHistoryEntry[],
+        newTop: GlobalEntry[],
+        type: LeaderboardType,
+        mode: ScoringMode
+    ) {
+        let has_june_1st_patch = database.get(`june_1st_patch:${type}:${mode}`);
+
         let time = 300 * Math.floor(DateTime.now().toSeconds() / 300);
+
+        if (!has_june_1st_patch) {
+            console.log(
+                `[CacheManager] Applying JUNE1ST Patch to global history ${type} / ${mode}`
+            );
+            history = history.filter(
+                (h) => h.time > this.JUNE_1ST_CUTOFF || h.value > this.JUNE_1ST_FILTER
+            );
+            database.putSync(`june_1st_patch:${type}:${mode}`, Date.now());
+        }
 
         let latest_history_scores: Map<string, GlobalHistoryEntry> = new Map();
         for (const entry of history) {
